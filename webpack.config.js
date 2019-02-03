6edc45337d63af370760d4c819e04cbca465ba1c
oiclid <neightman@gmail.com>
2019-02-03 10:39:17 +0100

    changes to wbepack.config, package and bablrc files. created src and public directories

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+import path from 'path';
+
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+module.exports = { 
+    entry: path.join(__dirname, 'src', 'app.js'),
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js' // this the compiled final javascript file which we will include in the index.html
+    },
+    mode: process.env.NODE_ENV || 'development',
+    resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules']
+    },
+    module: { 
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: ['babel-loader']
+        },
+        {
+            test: /\.(css|scss)$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]              
+        },
+        {
+            test: /\.(jpg|jpeg|png|gif|mp3|mp4|mov|avi|wmv|wma|svg|ogg)$/,
+            loaders: ['file=loader']
+        }
+    ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ],
+    devtool: 'cheap-module-eval-source-map', // this helps the browser to point ot the exact file in the console, helps in debugging
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        historyApiFallback: true 
+    }
+}
